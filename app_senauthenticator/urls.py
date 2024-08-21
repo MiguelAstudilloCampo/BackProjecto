600f3303ee6d39badda5cbef0d60f832da84a098
MiguelAstudilloCampo <miguelangelastudillo38@gmail.com>
2024-08-21 16:34:57 -0500

    primer commit

---
+++ app_senauthenticator/urls.py
@@ -0,0 +1,33 @@
+from django.urls import path
+from app_senauthenticator.controllers import programa, ficha, usuario, registro_facial, objeto, ingreso, tutor
+# from app_senauthenticator.controllers.autenticacion_facial import AutenticacionFacial
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('programa/', programa.programa_controlador),
+    path('programa/<int:pk>/', programa.programa_controlador),
+    path('ficha/', ficha.ficha_controlador),
+    path('ficha/<int:pk>/', ficha.ficha_controlador),
+    path('usuario/', usuario.usuario_controlador),
+    path('usuario/<int:pk>/', usuario.usuario_controlador),
+    path('registroFacial/', registro_facial.registro_facial_controlador),
+    path('registroFacial/<int:pk>/', registro_facial.registro_facial_controlador),
+    path('objeto/', objeto.objeto_controlador),
+    path('objeto/<int:pk>/', objeto.objeto_controlador),
+    path('tutor/', tutor.tutor_controlador),
+    path('tutor/<int:pk>/', tutor.tutor_controlador),
+    path('ingreso/', ingreso.ingreso_controlador),
+    path('ingreso/<int:pk>/', ingreso.ingreso_controlador),
+    path('inicioSesion/', usuario.inicio_sesion),
+    path('perfil/', usuario.perfil),
+    # path('autenticacionFacial/', AutenticacionFacial.as_view())
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
